ac7621c39b8cf602dc325ddc46428f9263ac5992
meredithheller <meredithheller@outlook.com>
2018-08-14 16:36:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Meredith"
+  end
+  get '/hometown' do
+    "My hometown is Bloomington, IL"
+  end
+  get '/favorite-song' do
+    "My favorite song is All Night"
+  end
 end
