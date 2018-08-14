91256d125af639fe24c6c4bda348cbe538cb3f24
meredithheller <meredithheller@outlook.com>
2018-08-14 16:34:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/name' do
     "My name is Meredith"
+  end
+  get '/hometown' do
+    "My hometown is Bloomington, IL"
+  end
+  get '/favorite-song' do
+    "My favorite song is All Night"
+  end
 end
